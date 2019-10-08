40643a1923724d329a8d2150a5a351c34e0a6a47
naserhamitou98 <naserhamitou98@gmail.com>
2019-10-07 22:14:04 -0400

    comment utiliser hashmap dans part 2

--- tp2_Maps/src/tp2/Interview.java
+++ tp2_Maps/src/tp2/Interview.java
@@ -16,13 +16,26 @@ public class Interview {
         //Initialisation de collection des MatchingPairs
         Collection<MatchingPair> pairs = new ArrayList<MatchingPair>();
 
+
+        //--------------------------  Comment utiliser hashmap ?  --------------------------------//
+
+
         //Iterateur
-        Iterator<Integer> it1 = values.iterator();
-        Iterator<Integer> it2 = values.iterator();
+        Iterator<Integer> it = values.iterator();
 
-        if(!values.isEmpty()) { //Verifie qu'il y a des valeurs a match
+        if(!values.isEmpty()) {
 
-            while (it1.hasNext()) {
+           while (it.hasNext()){
+
+           }
+
+        }
+
+        return pairs;
+
+        //Code non utiliser
+        /*
+        while (it1.hasNext()) {
                 it2 = it1;
                 int val1 = it1.next();
                 while (it2.hasNext()) {
@@ -31,9 +44,8 @@ public class Interview {
                         pairs.add(new MatchingPair(val1, val2));
                 }
             }
-        }
-
-        return pairs;
+        */
+        /////////////////////////////////////////////////
         /*
         while (it.hasNext()){
             int val1 = it.next();
@@ -46,6 +58,21 @@ public class Interview {
 
 
         }*/
+    }
+
+
+    //Node Definition
+    static class Node<Value1, Value2> {
+        final Value1 key;
+        Value2 data;
+        LinkedHashMap.Node next; // Pointer to the next node within a Linked List
+
+        Node(Value1 key, Value2 data)
+        {
+            this.key = key;
+            this.data = data;
+            next = null;
+        }
     }
 
 }
